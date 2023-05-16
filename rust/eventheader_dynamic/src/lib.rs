81b95dd9926b983ea90c84b74f3a7029064227ba
Doug Cook (WINDOWS) <dcook@microsoft.com>
2023-05-15 19:45:12 -0700

    Docs

--- rust/eventheader_dynamic/src/lib.rs
+++ rust/eventheader_dynamic/src/lib.rs
@@ -14,6 +14,15 @@
 //! filesystem to be mounted, and appropriate permissions configured for the
 //! `/sys/kernel/.../tracing/user_events_data` file).
 //!
+//! This "dynamic" implementation is more flexible than the implementation in the
+//! [`eventheader`] crate. For example, it supports runtime-defined schema and can
+//! easily log arrays of strings. However, it is harder to use, it has higher runtime
+//! costs, and it depends on the `alloc` crate. This dynamic implementation is intended
+//!  for use only when the set of events cannot be determined at compile-time. For
+//! example, `eventheader_dynamic` might be used to implement a middle-layer library
+//! providing tracing support to a scripting language like JavaScript or
+//! Python. In other cases, use the [`eventheader`] crate instead of this crate.
+//! 
 //! # Overview
 //!
 //! - Create a [Provider] object with the name of the provider to be used.
